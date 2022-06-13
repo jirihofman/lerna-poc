0813635f932805457066bb1d593df063c3820a47
Jiří Hofman <jiri.hofman@gmail.com>
2022-06-13 11:30:40 +0200

    monolithic repo

---
+++ app/index.js
@@ -0,0 +1,16 @@
+console.log("Starting App");
+
+const model = require('./model');
+const snap = require('./snap');
+console.log("Model: ", model.hello());
+console.log("SNAP: ", snap.hello());
+
+// console.log("--- monorepo start ---")
+// const monorepoModel = require('@jirihofman/model');
+// const monorepoSnap = require('@jirihofman/snap');
+// console.log("Model: ", monorepoModel.hello());
+// console.log("SNAP: ", monorepoSnap.hello());
+
+// console.log("--- monorepo end -----")
+
+console.log("App run completed");
